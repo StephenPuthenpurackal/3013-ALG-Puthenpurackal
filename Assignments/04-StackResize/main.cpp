cdcf03ce9cbbce5f517d32b7f77776fad6234354
StephenPuthenpurackal <sjputhenpurack1225@my.msutexas.edu>
2020-02-19 05:12:07 -0600

    changed size to newsize

--- Assignments/04-StackResize/main.cpp
+++ Assignments/04-StackResize/main.cpp
@@ -258,7 +258,7 @@ public:
         int newSize = size * 1.75;    // double size of original
         int *B = new int[newSize]; // allocate new memory
 
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < newSize; i++)
         { // copy values to new array
             B[i] = A[i];
         }
@@ -278,7 +278,7 @@ public:
          int newSize = size -0.5;    // double size of original
         int *B = new int[newSize]; // allocate new memory
 
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < newSize; i++)
         { // copy values to new array
             B[i] = A[i];
         }
